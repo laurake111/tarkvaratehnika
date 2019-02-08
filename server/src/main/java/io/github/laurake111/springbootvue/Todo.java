307a43639f5e3f6ffc8662b70573068c85b10bef
Laura <lleisb@ttu.ee>
2019-02-08 20:55:53 +0200

    second

---
+++ server/src/main/java/io/github/laurake111/springbootvue/Todo.java
@@ -0,0 +1,22 @@
+package io.github.laurake111.springbootvue;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Todo {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    private Boolean completed = false;
+
+}
